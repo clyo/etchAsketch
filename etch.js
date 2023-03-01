2e128d8e74cbd370d409498a76179ee2008ea50b
Clyo <colm-l@hotmail.com>
2023-03-01 11:34:17 -0500

    Adding etch.js, style.css and index.html
    This commit has the 3 files that has a functioning etch a sketch
    but has further work needed to get user input on grid size

---
+++ etch.js
@@ -0,0 +1,39 @@
+const grid = document.getElementById("container");
+let rows = document.getElementsByClassName("gridRows");
+let cells = document.getElementsByClassName("cell");
+
+
+function makeRows(rowNum){
+
+    //Creates rows
+    for (r = 0; r < rowNum; r++){
+        let row = document.createElement("div");
+        grid.appendChild(row).className = "gridRows";
+    };
+};
+
+function makeColumns(cellNum){
+
+    //Creates columns
+    for (i = 0; i < rows.length; i++){
+        for(j = 0; j < cellNum; j++){
+            let newCell = document.createElement("div");
+            rows[j].appendChild(newCell).className = "cell";
+        };
+    };
+};
+
+makeRows(16);
+makeColumns(16);
+
+let cell = document.getElementsByClassName("cell");
+console.log(cell);
+
+grid.addEventListener('mouseover', trail);
+    
+
+
+function trail(e) {
+    e.target.classList.add('trail');
+}
+
